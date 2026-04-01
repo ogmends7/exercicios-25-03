19f30dc95f551771460eeed5e6ef076f3ad821ff
ogmends7 <gustavogtmendes@gmail.com>
2026-04-01 09:34:30 -0300

    Add files via upload

---
+++ exercicio5.java
@@ -0,0 +1,10 @@
+public class exercicio5 {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while (contador <=10) {
+            System.out.printf("%n5 x %d = %d" , contador, contador * 5);
+            contador ++;
+        }
+    }
+}
